a02facd288153c56fdc6018075f9f25e8ae64239
Eneojo <amehsunday078@gmail.com>
2025-01-29 13:03:44 +0100

    types

--- src/pageSections/SectionOne.js
+++ src/pageSections/SectionOne.js
@@ -65,7 +65,7 @@ function SectionOne() {
                     <div className='cd-text'>
                       <small>{recentItem.property_name}</small>
                       <h4>{recentItem.type}</h4>
-                      <h4>{recentItem.description}</h4>
+                      {/* <h4>{recentItem.description}</h4> */}
                       <br />
                       <p>{recentItem.size || 'Short description of the property'}</p>
                       <div className='apt-deatails'>
